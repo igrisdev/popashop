722e17d7ee9bd48c1156e2d1a60e335fb6ceb320
TJIGRIS27m <johanalvarezzz237@gmail.com>
2024-03-18 18:11:34 -0500

    Se agruparon los componentes de acciones de creación, en una sola carpeta

--- src/app/(pages)/dashboard/page.jsx
+++ src/app/(pages)/dashboard/page.jsx
@@ -1,8 +1,9 @@
 'use client'
 
 import { Plus } from 'lucide-react'
-import { DialogCreateProduct } from '@/components/DialogCreateProduct'
-import { DialogCreateCategory } from '@/components/DialogCreateCategory'
+import { DialogCreateProduct } from '@/components/creationActions/DialogCreateProduct'
+import { DialogCreateCategory } from '@/components/creationActions/DialogCreateCategory'
+import { DialogCreateBrand } from '@/components/creationActions/DialogCreateBrand'
 
 const actions = [
   {
@@ -22,8 +23,15 @@ const actions = [
   {
     id: 3,
     title: 'Crear Color',
-    Component: DialogCreateProduct,
+    Component: DialogCreateBrand,
     description: 'Añadir un nuevo color a la tienda online PopaShop',
+    Icon: Plus,
+  },
+  {
+    id: 4,
+    title: 'Crear Marca',
+    Component: DialogCreateBrand,
+    description: 'Añadir una nueva marca a la tienda online PopaShop',
     Icon: Plus,
   },
 ]
@@ -45,36 +53,4 @@ export default function dashboard() {
       <section>data</section>
     </div>
   )
-}
-
-/* const actions = [
-  {
-    title: 'Crear Producto',
-    href: '/dashboard/product',
-    Icon: Plus,
-  },
-  {
-    title: 'Crear Categoría',
-    href: '/dashboard/category',
-    Icon: Plus,
-  },
-  {
-    title: 'Crear Color',
-    href: '/dashboard/color',
-    Icon: Plus,
-  },
-] */
-
-{
-  /* {actions.map(({ title, href, Icon }) => (
-  <li key={`${title}-${href}`}>
-  <Link
-  href={href}
-  className={buttonVariants({ variant: 'default' })}
-  >
-  <p>{title}</p>
-  {<Icon />}
-  </Link>
-  </li>
-))} */
 }
---
+++ src/components/creationActions/DialogCreateBrand.jsx
@@ -0,0 +1,29 @@
+import { Button } from '@/components/ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+
+import { FormCreateBrand } from './FormCreateBrand'
+
+export const DialogCreateBrand = ({ title, description }) => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant='outline'>{title}</Button>
+      </DialogTrigger>
+      <DialogContent className='sm:max-w-[525px]'>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+
+        <FormCreateBrand />
+      </DialogContent>
+    </Dialog>
+  )
+}
