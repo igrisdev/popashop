e791b02ab7ac694236a098c94e08d55d761dbbef
TJIGRIS27m <johanalvarezzz237@gmail.com>
2024-04-02 22:19:40 -0500

    prueba n2: sin mi axios

--- src/app/(pages)/dashboard/products/page.jsx
+++ src/app/(pages)/dashboard/products/page.jsx
@@ -1,15 +1,20 @@
-import axios from '@/lib/axios'
+// import axios from '@/lib/axios'
+import axios from 'axios'
 
 export default async function page() {
-  const res = await axios.get('/api/category')
+  // const res = await axios.get('/api/category')
+  const res = await axios.get('http://localhost:3000/api/category')
+
+  console.log(res)
 
   const { data } = res
+  // const data = res
 
   return (
     <div>
       {data?.map((item) => (
         <div key={item.id}>
-          <p>{item.name}</p>
+          <p>{item.id}</p>
         </div>
       ))}
     </div>
