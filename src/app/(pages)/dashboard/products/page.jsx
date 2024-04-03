4cd6c3574b51075058bae3ccdd376bf624cc82aa
TJIGRIS27m <johanalvarezzz237@gmail.com>
2024-04-02 22:21:52 -0500

    prueba n3: usando fetch

--- src/app/(pages)/dashboard/products/page.jsx
+++ src/app/(pages)/dashboard/products/page.jsx
@@ -1,14 +1,20 @@
 // import axios from '@/lib/axios'
 import axios from 'axios'
 
+// n3
+
 export default async function page() {
   // const res = await axios.get('/api/category')
-  const res = await axios.get('http://localhost:3000/api/category')
+  // const res = await axios.get('http://localhost:3000/api/category')
+
+  const res = await fetch('http://localhost:3000/api/category', {
+    method: 'GET',
+  }).then((res) => res.json())
 
   console.log(res)
 
-  const { data } = res
-  // const data = res
+  // const { data } = res
+  const data = res
 
   return (
     <div>
