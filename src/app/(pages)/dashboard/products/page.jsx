8cb010ac13c9d457389c09bd43997b397521a678
TJIGRIS27m <johanalvarezzz237@gmail.com>
2024-04-02 22:26:02 -0500

    prueba n4: usando otra api

--- src/app/(pages)/dashboard/products/page.jsx
+++ src/app/(pages)/dashboard/products/page.jsx
@@ -1,24 +1,24 @@
 // import axios from '@/lib/axios'
-import axios from 'axios'
+// import axios from 'axios'
 
-// n3
+// n4
 
 export default async function page() {
   // const res = await axios.get('/api/category')
   // const res = await axios.get('http://localhost:3000/api/category')
 
-  const res = await fetch('http://localhost:3000/api/category', {
+  const res = await fetch('https://rickandmortyapi.com/api/character', {
     method: 'GET',
   }).then((res) => res.json())
 
   console.log(res)
 
   // const { data } = res
-  const data = res
+  const data = res.results
 
   return (
     <div>
-      {data?.map((item) => (
+      {data.map((item) => (
         <div key={item.id}>
           <p>{item.id}</p>
         </div>
